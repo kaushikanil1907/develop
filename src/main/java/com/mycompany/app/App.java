e2a77664651f9cf16d6e6da665fd8dad3bc7ae8a
akumar <kaushikanil.1907@gmail.com>
2019-05-09 05:37:55 -0400

    new Change

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World! Anil";
+    private final String message = "Hello World! Anil Kaushik";
 
     public App() {}
 
--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -34,9 +34,9 @@ public class AppTest
     {
         App.main(null);
         try {
-            assertEquals("Hello World! Anil" + System.getProperty("line.separator"), outContent.toString());
+            assertEquals("Hello World! Anil Kaushik" + System.getProperty("line.separator"), outContent.toString());
         } catch (AssertionError e) {
-            fail("\"message\" is not \"Hello World! Anil\"");
+            fail("\"message\" is not \"Hello World! Anil Kaushik\"");
         }
     }
 
