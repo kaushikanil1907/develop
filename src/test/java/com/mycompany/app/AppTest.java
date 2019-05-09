8a24364dbba0664c61175ddda7c05974790cb05e
akumar <kaushikanil.1907@gmail.com>
2019-05-09 05:47:45 -0400

    delete prev change

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -34,9 +34,9 @@ public class AppTest
     {
         App.main(null);
         try {
-            assertEquals("Hello World! Anil Kaushik" + System.getProperty("line.separator"), outContent.toString());
+            assertEquals("Hello World! Anil K" + System.getProperty("line.separator"), outContent.toString());
         } catch (AssertionError e) {
-            fail("\"message\" is not \"Hello World! Anil Kaushik\"");
+            fail("\"message\" is not \"Hello World! Anil K\"");
         }
     }
 
